01e7489809d77c8be459e6e8e025bf8006ec138e
Evgen Kyselgov <kyselgov@gmail.com>
2025-09-16 10:36:04 +0300

    use OxyPlot instead of ListBox

--- DataUI/UI/ViewModels/MainWindowViewModel.cs
+++ DataUI/UI/ViewModels/MainWindowViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using DataSrvLib;
-using System.Collections.ObjectModel;
+using OxyPlot;
+using OxyPlot.Axes;
+using OxyPlot.Series;
 using System.Windows;
 
 namespace DataUI.UI.ViewModels
@@ -8,9 +10,11 @@ namespace DataUI.UI.ViewModels
     {
         #region Properties
 
-        public ObservableCollection<string> Samples { get; set; } = new ObservableCollection<string>();
+        public PlotModel PlotModel {  get; init; } = new PlotModel();
 
         CPUTempData? CPUProvider { get; set; } = default;
+
+        IList<float> Values { get; set; } = new List<float>();
 
         #endregion
 
@@ -18,6 +22,9 @@ namespace DataUI.UI.ViewModels
 
         public void Initialize()
         {
+            Values = Enumerable.Repeat(0.0f, 100).ToList();
+            BuildPlotModel();
+
             var obj = new DataProvider();
 
             CPUProvider = obj.CpuTemp;
@@ -39,15 +46,49 @@ namespace DataUI.UI.ViewModels
 
         void OnNextSample(float value)
         {
-            Application.Current.Dispatcher.Invoke(() =>
-            {
-                Samples.Add($"{value:F2}");
+            Application.Current.Dispatcher.Invoke(() => AddSample(value));
+        }
 
-                if (Samples.Count > 7)
-                {
-                    Samples.RemoveAt(0);
-                }
-            });
+        #endregion
+
+        #region Helper Methods
+
+        void BuildPlotModel()
+        {
+            var series = new LinearBarSeries();
+            series.FillColor = OxyColor.FromArgb(69, 76, 175, 80);
+            series.StrokeColor = OxyColor.FromArgb(255, 76, 175, 80);
+            series.StrokeThickness = 1;
+            series.Points.AddRange(Values.Select((v, i)=> new DataPoint(i, v)));
+
+            var axisTemp = new LinearAxis();
+            axisTemp.Position = AxisPosition.Left;
+            axisTemp.Minimum = 0;
+            axisTemp.Maximum = 100;
+            axisTemp.MajorGridlineStyle = LineStyle.Solid;
+            axisTemp.MinorGridlineStyle = LineStyle.Dot;
+            axisTemp.IsZoomEnabled = false;
+
+            var axisSample = new LinearAxis();
+            axisSample.Position = AxisPosition.Bottom;
+            axisSample.TickStyle = TickStyle.None;
+            axisSample.IsAxisVisible = false;
+
+            PlotModel.Axes.Add(axisTemp);
+            PlotModel.Axes.Add(axisSample);
+            PlotModel.Series.Add(series);
+        }
+
+        void AddSample(float value)
+        {
+            Values.RemoveAt(0);
+            Values.Add(value);
+
+            var series = PlotModel.Series.Cast<LinearBarSeries>().First()!;
+            series.Points.Clear();
+            series.Points.AddRange(Values.Select((v, i) => new DataPoint(i, v)));
+
+            PlotModel.InvalidatePlot(true);
         }
 
         #endregion
