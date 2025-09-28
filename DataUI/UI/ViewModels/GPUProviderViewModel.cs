ba7950044796c6897ac56a8a0a82443fe893b9fb
Evgen Kyselgov <kyselgov@gmail.com>
2025-09-28 18:23:15 +0300

    add UI

---
+++ DataUI/UI/ViewModels/GPUProviderViewModel.cs
@@ -0,0 +1,223 @@
+﻿using DataSrvLib;
+using DataUI.UI.Converters;
+using OxyPlot;
+using OxyPlot.Axes;
+using OxyPlot.Series;
+using System.Diagnostics;
+using System.Windows.Media;
+using Wpf.Ui.Markup;
+
+namespace DataUI.UI.ViewModels
+{
+    public class GPUProviderViewModel
+    {
+        #region Properties
+
+        public IWindowViewModelHost? Host { get; private set; }
+
+        public GPUTempData? Provider { get; private set; } = default;
+
+        public PlotModel Model { get; init; } = new PlotModel();
+
+        #endregion
+
+        #region Construction
+
+        internal static GPUProviderViewModel Create(GPUTempData provider, IWindowViewModelHost? host)
+        {
+            var res = new GPUProviderViewModel()
+            {
+                Provider = provider,
+                Host = host
+            };
+
+            res.Init();
+
+            return res;
+        }
+
+        internal void Init()
+        {
+            if (Host != default)
+            {
+                Host.ThemeChanged += OnThemeChanged;
+            }
+
+            if (Provider != default)
+            {
+                Provider.OnNextSample += OnNextSample;
+            }
+
+            BuildPlotModel();
+            BindPlotColors();
+        }
+
+        internal void Clean()
+        {
+            if (Provider != default)
+            {
+                Provider.OnNextSample -= OnNextSample;
+            }
+
+            if (Host != default)
+            {
+                Host.ThemeChanged -= OnThemeChanged;
+            }
+
+            Provider = default;
+        }
+
+        #endregion
+
+        #region Event Handlers
+
+        void OnThemeChanged(Wpf.Ui.Appearance.ApplicationTheme currentApplicationTheme, Color systemAccent)
+        {
+            BindPlotColors();
+        }
+
+        void OnNextSample(float value)
+        {
+            Host?.Dispatcher.Invoke(() => AddSample(value));
+        }
+
+        #endregion
+
+        #region Helper Methods
+
+        void BuildPlotModel()
+        {
+            var series = new AreaSeries();
+            series.StrokeThickness = 1;
+
+            var axisTemp = new LinearAxis();
+            axisTemp.Position = AxisPosition.Left;
+            axisTemp.Minimum = 0;
+            axisTemp.Maximum = c_maxTemp;
+            axisTemp.MajorGridlineStyle = LineStyle.Solid;
+            axisTemp.MinorGridlineStyle = LineStyle.Solid;
+            axisTemp.IsPanEnabled = false;
+            axisTemp.IsZoomEnabled = false;
+
+            var axisSample = new LinearAxis();
+            axisSample.Position = AxisPosition.Bottom;
+            axisSample.Minimum = 0;
+            axisSample.Maximum = c_maxCount - 1;
+            axisSample.TickStyle = TickStyle.None;
+            axisSample.IsPanEnabled = false;
+            axisSample.IsZoomEnabled = false;
+            axisSample.IsAxisVisible = false;
+
+            Model.IsLegendVisible = false;
+            Model.Axes.Add(axisTemp);
+            Model.Axes.Add(axisSample);
+            Model.Series.Add(series);
+
+            BindPlotColors();
+            BindTextTitels();
+
+            Model.InvalidatePlot(true);
+        }
+
+        void BindPlotColors()
+        {
+            if (Host == default)
+            {
+                return;
+            }
+
+            var converter = new ColorToOxyPlotColorConverter();
+            var background = converter.Convert((Color)Host.ApplicationResources[ThemeResource.ApplicationBackgroundColor.ToString()]);
+            var text = converter.Convert((Color)Host.ApplicationResources[ThemeResource.TextFillColorPrimary.ToString()]);
+            var title = converter.Convert((Color)Host.ApplicationResources[ThemeResource.TextFillColorSecondary.ToString()]);
+            var subTitle = converter.Convert((Color)Host.ApplicationResources[ThemeResource.TextFillColorTertiary.ToString()]);
+            var majorGrid = converter.Convert((Color)Host.ApplicationResources[ThemeResource.TextFillColorTertiary.ToString()]);
+            var minorGrid = OxyColor.FromAColor(0x30, majorGrid);
+            var seriesStroke = converter.Convert((Color)Host.ApplicationResources[ThemeResource.SystemAccentColorTertiary.ToString()]);
+            var seriesFill = OxyColor.FromAColor(0x30, seriesStroke);
+
+            Model.Background = background;
+            Model.PlotAreaBackground = background;
+            Model.PlotAreaBorderColor = OxyColors.Undefined;
+            Model.TextColor = text;
+            Model.TitleColor = title;
+            Model.SubtitleColor = subTitle;
+
+            var axisTemp = Model.Axes.Cast<LinearAxis>()
+                                         .FirstOrDefault(a => a.Position == AxisPosition.Left);
+            if (axisTemp != default)
+            {
+                axisTemp.TitleColor = title;
+                axisTemp.MajorGridlineColor = majorGrid;
+                axisTemp.MinorGridlineColor = minorGrid;
+            }
+
+            var series = Model.Series.ElementAtOrDefault(0) as AreaSeries;
+            if (series != default)
+            {
+                series.Color = seriesStroke;
+                series.Fill = seriesFill;
+            }
+        }
+
+        void BindTextTitels()
+        {
+            if (Host == default)
+            {
+                return;
+            }
+
+            var axisTemp = Model.Axes.Cast<LinearAxis>()
+                                         .FirstOrDefault(a => a.Position == AxisPosition.Left);
+            if (axisTemp != default)
+            {
+                var providerName = Provider?.Name ?? string.Empty;
+
+                axisTemp.Title = providerName;
+                axisTemp.Unit = (string)Host.ApplicationResources["CelsiusUnitLabel"];
+            }
+        }
+
+        void AddSample(float value)
+        {
+            var series = Model.Series.FirstOrDefault() as AreaSeries;
+            if (series != default)
+            {
+                var axisTemp = (LinearAxis)Model.Axes.ElementAt(0);
+                var axisSample = (LinearAxis)Model.Axes.ElementAt(1);
+                Debug.Assert(axisTemp.Position == AxisPosition.Left);
+                Debug.Assert(axisSample.Position == AxisPosition.Bottom);
+
+                var shiftData = series.Points.Count >= c_maxCount;
+
+                // new sample
+                var sample = new DataPoint(shiftData ? axisSample.Maximum + 1 : series.Points.Count, value);
+                series.Points.Add(sample);
+
+                // scroll graph and remove oldest sample
+                if (shiftData)
+                {
+                    axisSample.Minimum += 1;
+                    axisSample.Maximum += 1;
+
+                    series.Points.RemoveRange(0, series.Points.Count - c_maxCount);
+                }
+
+                // tune low temp scale
+                var minValue = series.Points.Select(dp => dp.Y).Min();
+                axisTemp.Minimum = Math.Floor(Math.Max(minValue - 30, 0) / 10) * 10;
+            }
+
+            Model.InvalidatePlot(true);
+        }
+
+        #endregion
+
+        #region Constants
+
+        const int c_maxCount = 120;
+        const double c_maxTemp = 100;
+
+        #endregion
+    }
+}
--- DataUI/UI/ViewModels/MainWindowViewModel.cs
+++ DataUI/UI/ViewModels/MainWindowViewModel.cs
@@ -1,11 +1,6 @@
 ﻿using DataSrvLib;
-using DataUI.UI.Converters;
 using OxyPlot;
-using OxyPlot.Axes;
-using OxyPlot.Series;
 using System.Diagnostics;
-using System.Windows.Media;
-using Wpf.Ui.Markup;
 
 namespace DataUI.UI.ViewModels
 {
@@ -13,37 +8,19 @@ namespace DataUI.UI.ViewModels
     {
         #region Properties
 
-        public PlotModel PlotModel { get; init; } = new PlotModel();
+        public PlotModel CPUPlotModel { get; init; } = new PlotModel();
 
-        public IEnumerable<SourceViewModel> Sources => GetSourceViewModels();
+        public CPUProviderViewModel? CPU { get; private set; } = default;
 
-        public SourceType Source
-        {
-            get => CPUProvider?.Source ?? SourceType.PDH;
-            set
-            {
-                if (CPUProvider == default)
-                {
-                    return;
-                }
+        public IEnumerable<GPUProviderViewModel> GPUs { get; private set; } = Enumerable.Empty<GPUProviderViewModel>();
 
-                if (CPUProvider.Source != value)
-                {
-                    CPUProvider.Source = value;
-                    NotifyPropertyChanged();
-                }
-            }
-        }
-
-        IWindowViewModelHost? Host { get; set; } = default;
-
-        CPUTempData? CPUProvider { get; set; } = default;
+        IMainWindowViewModelHost? Host { get; set; } = default;
 
         #endregion
 
         #region Construction
 
-        public void Initialize(IWindowViewModelHost host)
+        public void Initialize(IMainWindowViewModelHost host)
         {
             Debug.Assert(host != default);
 
@@ -52,27 +29,28 @@ namespace DataUI.UI.ViewModels
             if (Host != default)
             {
                 Host.Loaded += OnLoadedAsync;
-                Host.ThemeChanged += OnThemeChanged;
             }
-
-            BuildPlotModel();
-
-            NotifyPropertyChanged(nameof(Sources),
-                                  nameof(Source));
         }
 
         public void Dispose()
         {
-            if (CPUProvider != default)
+            CPU?.Clean();
+            CPU = default;
+
+            if (GPUs.Any())
             {
-                CPUProvider.OnNextSample -= OnNextSample;
-                CPUProvider = default;
+                foreach(var gpu in GPUs)
+                {
+                    gpu.Clean();
+                }
+
+                GPUs = Enumerable.Empty<GPUProviderViewModel>();
             }
+
 
             if (Host != default)
             {
                 Host.Loaded -= OnLoadedAsync;
-                Host.ThemeChanged -= OnThemeChanged;
                 Host = default;
             }
         }
@@ -86,27 +64,9 @@ namespace DataUI.UI.ViewModels
             await BuildDataProviderAsync();
         }
 
-        void OnThemeChanged(Wpf.Ui.Appearance.ApplicationTheme currentApplicationTheme, Color systemAccent)
-        {
-            BindPlotColors();
-        }
-
-        void OnNextSample(float value)
-        {
-            Host?.Dispatcher.Invoke(() => AddSample(value));
-        }
-
         #endregion
 
         #region Helper Methods
-
-        IEnumerable<SourceViewModel> GetSourceViewModels()
-        {
-            var res = new[] { SourceType.PDH, SourceType.WMI }
-                      .Select(s => new SourceViewModel() { Source = s, Host = Host })
-                      .ToList();
-            return res;
-        }
 
         async Task BuildDataProviderAsync()
         {
@@ -114,20 +74,21 @@ namespace DataUI.UI.ViewModels
             {
                 var dataProvider = new DataProvider();
 
-                CPUProvider = dataProvider.CpuTemp;
-                CPUProvider.Source = SourceType.PDH;
-                CPUProvider.OnNextSample += OnNextSample;
-
-                var gpuProvider = dataProvider.GpuTemp;
-
-#if DEBUG
-                foreach (var gpu in gpuProvider.Cast<GPUTempData>())
+                if (CPU == default)
                 {
-                    Debug.WriteLine($"{gpu.Index} - {gpu.Name}");
+                    CPU = CPUProviderViewModel.Create(dataProvider.CpuTemp, Host);
+                    NotifyPropertyChanged(nameof(CPU));
                 }
-#endif
-                NotifyPropertyChanged(nameof(Sources),
-                                      nameof(Source));
+
+                if (!GPUs.Any())
+                {
+                    GPUs = dataProvider.GpuTemp.Cast<GPUTempData>()
+                                               .Select(gpu => GPUProviderViewModel.Create(gpu, Host))
+                                               .ToList();
+                    NotifyPropertyChanged(nameof(GPUs));
+                }
+
+                Host?.BuildGridLayout();
             }
             catch(Exception ex)
             {
@@ -137,130 +98,6 @@ namespace DataUI.UI.ViewModels
                                                    $"{(string)Host.ApplicationResources["ProviderFailedLabel"]}\n{ex.Message}");
                 }
             }
-        }
-
-        void BuildPlotModel()
-        {
-            var series = new AreaSeries();
-            series.StrokeThickness = 1;
-
-            var axisTemp = new LinearAxis();
-            axisTemp.Position = AxisPosition.Left;
-            axisTemp.Minimum = 0;
-            axisTemp.Maximum = c_maxTemp;
-            axisTemp.MajorGridlineStyle = LineStyle.Solid;
-            axisTemp.MinorGridlineStyle = LineStyle.Solid;
-            axisTemp.IsPanEnabled = false;
-            axisTemp.IsZoomEnabled = false;
-
-            var axisSample = new LinearAxis();
-            axisSample.Position = AxisPosition.Bottom;
-            axisSample.Minimum = 0;
-            axisSample.Maximum = c_maxCount - 1;
-            axisSample.TickStyle = TickStyle.None;
-            axisSample.IsPanEnabled = false;
-            axisSample.IsZoomEnabled = false;
-            axisSample.IsAxisVisible = false;
-
-            PlotModel.IsLegendVisible = false;
-            PlotModel.Axes.Add(axisTemp);
-            PlotModel.Axes.Add(axisSample);
-            PlotModel.Series.Add(series);
-
-            BindPlotColors();
-            BindTextTitels();
-
-            PlotModel.InvalidatePlot(true);
-        }
-
-        void BindPlotColors()
-        {
-            if (Host == default)
-            {
-                return;
-            }
-
-            var converter = new ColorToOxyPlotColorConverter();
-            var background = converter.Convert((Color)Host.ApplicationResources[ThemeResource.ApplicationBackgroundColor.ToString()]);
-            var text = converter.Convert((Color)Host.ApplicationResources[ThemeResource.TextFillColorPrimary.ToString()]);
-            var title = converter.Convert((Color)Host.ApplicationResources[ThemeResource.TextFillColorSecondary.ToString()]);
-            var subTitle = converter.Convert((Color)Host.ApplicationResources[ThemeResource.TextFillColorTertiary.ToString()]);
-            var majorGrid = converter.Convert((Color)Host.ApplicationResources[ThemeResource.TextFillColorTertiary.ToString()]);
-            var minorGrid = OxyColor.FromAColor(0x30, majorGrid);
-            var seriesStroke = converter.Convert((Color)Host.ApplicationResources[ThemeResource.SystemAccentColorPrimary.ToString()]);
-            var seriesFill = OxyColor.FromAColor(0x30, seriesStroke);
-
-            PlotModel.Background = background;
-            PlotModel.PlotAreaBackground = background;
-            PlotModel.PlotAreaBorderColor = OxyColors.Undefined;
-            PlotModel.TextColor = text;
-            PlotModel.TitleColor = title;
-            PlotModel.SubtitleColor = subTitle;
-
-            var axisTemp = PlotModel.Axes.Cast<LinearAxis>()
-                                         .FirstOrDefault(a => a.Position == AxisPosition.Left);
-            if (axisTemp != default)
-            {
-                axisTemp.TitleColor = title;
-                axisTemp.MajorGridlineColor = majorGrid;
-                axisTemp.MinorGridlineColor = minorGrid;
-            }
-
-            var series = PlotModel.Series.ElementAtOrDefault(0) as AreaSeries;
-            if (series != default)
-            {
-                series.Color = seriesStroke;
-                series.Fill = seriesFill;
-            }
-        }
-
-        void BindTextTitels()
-        {
-            if (Host == default)
-            {
-                return;
-            }
-
-            var axisTemp = PlotModel.Axes.Cast<LinearAxis>()
-                                         .FirstOrDefault(a => a.Position == AxisPosition.Left);
-            if (axisTemp != default)
-            {
-                axisTemp.Title = (string)Host.ApplicationResources["TemperatureLabel"];
-                axisTemp.Unit = (string)Host.ApplicationResources["CelsiusUnitLabel"];
-            }
-        }
-
-        void AddSample(float value)
-        {
-            var series = PlotModel.Series.FirstOrDefault() as AreaSeries;
-            if (series != default)
-            {
-                var axisTemp = (LinearAxis)PlotModel.Axes.ElementAt(0);
-                var axisSample = (LinearAxis)PlotModel.Axes.ElementAt(1);
-                Debug.Assert(axisTemp.Position == AxisPosition.Left);
-                Debug.Assert(axisSample.Position == AxisPosition.Bottom);
-
-                var shiftData = series.Points.Count >= c_maxCount;
-
-                // new sample
-                var sample = new DataPoint(shiftData ? axisSample.Maximum + 1 : series.Points.Count, value);
-                series.Points.Add(sample);
-
-                // scroll graph and remove oldest sample
-                if (shiftData)
-                {
-                    axisSample.Minimum += 1;
-                    axisSample.Maximum += 1;
-
-                    series.Points.RemoveRange(0, series.Points.Count - c_maxCount);
-                }
-
-                // tune low temp scale
-                var minValue = series.Points.Select(dp => dp.Y).Min();
-                axisTemp.Minimum = Math.Floor(Math.Max(minValue - 30, 0) / 10) * 10;
-            }
-
-            PlotModel.InvalidatePlot(true);
         }
 
         #endregion
